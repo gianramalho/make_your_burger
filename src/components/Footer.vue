794afdbb02ef363be67e8d37a1bc7d1e217078d7
Gian Lucas <gian15249@gmail.com>
2022-02-28 22:43:59 +0000

    Finalizando cabeçado e rodapé do projeto - aula 22

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div id="footer">
-      Footer
-  </div>
+    <footer id="footer">
+      <p>Make Your Burger &copy; 2022</p>
+  </footer>
 </template>
 
 <script>
@@ -11,5 +11,16 @@ export default {
 </script>
 
 <style scoped>
-
+  #footer{
+    width: 100%;
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute; 
+    bottom: 0;
+  }
 </style>

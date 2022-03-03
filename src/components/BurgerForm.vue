fb8e5c49336790a07cdbe32c772a0b779ccac866
Gian Lucas <gian15249@gmail.com>
2022-03-03 22:46:05 +0000

    Criando formulário de cadastro - aula 24

---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <p>Componente de mensagem</p>
+    <div id="form-container">
+      <form id="burger-form">
+
+        <div class="input-container">
+          <label for="name">Nome do Cliente</label>
+          <input type="text" id="name" name="name" v-model="name" placeholder="Digite o seu nome">
+        </div>
+        <div class="input-container">
+          <label for="bread">Escolha o Pão do seu Burger:</label>
+          <select name="bread" id="bread" v-model="bread">
+            <option value="" selected>Escolha seu pão</option>
+          </select>
+        </div>
+        <div class="input-container">
+          <label for="beef">Escolha sua Carne do seu Burger:</label>
+          <select name="beef" id="beef" v-model="beef">
+            <option value="" selected>Escolha sua Carne</option>
+          </select>
+        </div>
+        <div class="input-container">
+          <label for="optionals">Escolha os opcionais:</label>
+          <div class="checkbox-container">
+            <input type="checkbox" id="optionals" name="optionals" v-model="optionals" value="salame">
+            <span>salame</span>
+          </div>
+        </div>
+        <div class="input-container">
+          <input type="submit" class="submit-btn" value="Criar meu Burger">
+        </div>
+
+      </form>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+
+export default{
+  name: "BurgerForm"
+}
+</script>
+
+<style scoped>
+  #form-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  #burger-form {
+    max-width: 400px;
+    margin: 0 auto;
+  }
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+  label {
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: #222;;
+    padding: 5px 10px;
+    border-left: 4px solid #fcba03;
+  }
+  input, select {
+    padding: 5px 10px;
+    width: 300px;
+  }
+  #opcionais-container {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  #opcionais-title {
+    width: 100%;
+  }
+  .checkbox-container {
+    display: flex;
+    align-items: flex-start;
+    width: 50%;
+    margin-bottom: 20px;
+  }
+  .checkbox-container span,
+  .checkbox-container input {
+    width: auto;
+  }
+  .checkbox-container span {
+    margin-left: 6px;
+    font-weight: bold;
+  }
+  .submit-btn {
+    background-color: #222;
+    color:#fcba03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: .5s;
+  }
+  .submit-btn:hover {
+    background-color: transparent;
+    color: #222;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
 <Banner />
   <div class="main-container">
     <h1>Monte o seu burger:</h1>
-    <p>formulario</p>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
   import Banner from "../components/Banner.vue";
+  import BurgerForm from "../components/BurgerForm.vue";
 
   export default{
     name: "Home",
     components:{
       Banner,
+      BurgerForm,
     }
   }
 </script>
